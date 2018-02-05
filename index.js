1675b4eac838319dac388d77c765069bfacec710
kiteenpilseneri <petri.leskinen@helsinki.fi>
2018-02-05 20:03:47 +0200

    Tehtävä 2.1
    valmis

---
+++ index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Sisalto = (props) => {
+  return (
+    <div>
+       {props.osat.map((osa)=><div key={osa.id}>{osa.nimi} {osa.tehtavia}</div>)}
+    </div>
+  )
+}
+
+const Kurssi = (props) => {
+  return (
+    <div>
+      <h1>{props.kurssi.nimi}</h1>
+      <Sisalto osat={props.kurssi.osat} />
+    </div>
+  )
+}
+
+const App = () => {
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10,
+        id: 1
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7,
+        id: 2
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14,
+        id: 3
+      },
+      {
+        nimi: 'Tukiopetus',
+        tehtavia: 12,
+        id: 4
+      }
+    ]
+  }
+  return (
+    <div>
+      <Kurssi kurssi={kurssi} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
