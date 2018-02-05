6af0d5b104036c232b730c880e5cbe6b1e518272
kiteenpilseneri <petri.leskinen@helsinki.fi>
2018-02-05 20:19:00 +0200

    Tehtävä 2.4 korjaus

--- index.js
+++ index.js
@@ -2,28 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Kurssi from './kurssi'
 
-const Sisalto = (props) => {
-    return (
-      <div>
-         {props.osat.map((osa)=><div key={osa.id}>{osa.nimi} {osa.tehtavia}</div>)}
-         <div>Yhteensä: {props.osat.reduce((summa,osa)=>summa+osa.tehtavia,0)}</div>
-      </div>
-    )
-   
-  }
-  
- 
-const Kurssi = (props) => {
-    return (
-      <div>
-        <h1 id={props.kurssi.id}>{props.kurssi.nimi} </h1>
-        <Sisalto osat={props.kurssi.osat} />
-        <div>
-          
-        </div>
-      </div>
-    )
-  }
 const App = () => {
   const kurssit = [
     {
