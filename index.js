db9745746d76342e5cbbb991744bfd6751a04ba4
kiteenpilseneri <petri.leskinen@helsinki.fi>
2018-02-05 20:17:19 +0200

    Tehtävä 2.4
    valmnis

--- index.js
+++ index.js
@@ -1,57 +1,84 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Kurssi from './kurssi'
 
 const Sisalto = (props) => {
-  return (
-    <div>
-       {props.osat.map((osa)=><div key={osa.id}>{osa.nimi} {osa.tehtavia}</div>)}
-       <div>Yhteensä: {props.osat.reduce((summa,osa)=>summa+osa.tehtavia,0)}</div>
-    </div>
-  )
- 
-}
-
-const Kurssi = (props) => {
-  return (
-    <div>
-      <h1>{props.kurssi.nimi}</h1>
-      <Sisalto osat={props.kurssi.osat} />
+    return (
       <div>
-        
+         {props.osat.map((osa)=><div key={osa.id}>{osa.nimi} {osa.tehtavia}</div>)}
+         <div>Yhteensä: {props.osat.reduce((summa,osa)=>summa+osa.tehtavia,0)}</div>
       </div>
-    </div>
-  )
-}
-
-const App = () => {
-  const kurssi = {
-    nimi: 'Half Stack -sovelluskehitys',
-    osat: [
-      {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10,
-        id: 1
-      },
-      {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7,
-        id: 2
-      },
-      {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14,
-        id: 3
-      },
-      {
-        nimi: 'Tukiopetus',
-        tehtavia: 12,
-        id: 4
-      }
-    ]
+    )
+   
   }
+  
+ 
+const Kurssi = (props) => {
+    return (
+      <div>
+        <h1 id={props.kurssi.id}>{props.kurssi.nimi} </h1>
+        <Sisalto osat={props.kurssi.osat} />
+        <div>
+          
+        </div>
+      </div>
+    )
+  }
+const App = () => {
+  const kurssit = [
+    {
+      nimi: 'Half Stack -sovelluskehitys',
+      id: 1,
+      osat: [
+        {
+          nimi: 'Reactin perusteet',
+          tehtavia: 10,
+          id: 1
+        },
+        {
+          nimi: 'Tiedonvälitys propseilla',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Komponenttien tila',
+          tehtavia: 14,
+          id: 3
+        },
+        {
+          nimi: 'Tukiopetus',
+          tehtavia: 12,
+          id: 4
+        }
+      ]
+    },
+    {
+      nimi: 'Node.js',
+      id: 2,
+      osat: [
+        {
+          nimi: 'Routing',
+          tehtavia: 3,
+          id: 1
+        },
+        {
+          nimi: 'Middlewaret',
+          tehtavia: 7,
+          id: 2
+        },
+        {
+          nimi: 'Tukiopetus',
+          tehtavia: 4,
+          id: 3
+        }
+      ]
+    }
+  ]
+
+  const kurssiLista = () => kurssit.map((k)=><Kurssi kurssi={k} />)
   return (
     <div>
-      <Kurssi kurssi={kurssi} />
+            {kurssiLista()}
     </div>
   )
 }
